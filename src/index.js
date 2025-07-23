b438ea8267967620358c36a9f422c2b8fe007c85
GuilhermePaiter <guipaiter@gmail.com>
2025-07-23 17:31:56 -0300

    Players and Dice

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const player1 = {
+    nome :  "Mario",
+    velocidade : 4,
+    manobrabilidade : 3,
+    poder : 3,
+    pontos : 0,
+}
+
+const player2 = {
+    nome :  "Luigi",
+    velocidade : 3,
+    manobrabilidade : 4,
+    poder : 4,
+    pontos : 0,
+}
+
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1; 
+}
