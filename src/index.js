4bbf2b970189a3d974a944709a372fe6338e8443
GuilhermePaiter <guipaiter@gmail.com>
2025-07-24 18:10:56 -0300

    round and block

--- src/index.js
+++ src/index.js
@@ -18,8 +18,33 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1; 
 }
 
-async function playRaceEngine(player1, player2) {
 
+async function getRandomBlock() {
+    let random = Math.random()
+    let result
+
+    switch (true) {
+        case random < 0.33 :
+            result = "Reta"
+            break
+        case random < 0.66 :
+            result = "Curva"
+            break
+        default :
+            result = "Confronto"
+    }
+
+    return result
+}
+
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <=   5; round ++) {
+        console.log(`🏁 Rodada ${round}`)
+        
+        //sortear bloco
+        let block = await getRandomBlock()
+        console.log(`Bloco: ${block}`)
+    }
 }
 
 (async function main() {
